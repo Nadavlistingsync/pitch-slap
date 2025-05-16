4a6dcd887ec71c3270bec411cba8713b34e945bf
Nadavlistingsync <Benedeknadav@gmail.com>
2025-05-16 10:49:18 -0400

    Remove static export config to support API routes

--- next.config.js
+++ next.config.js
@@ -2,9 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  output: 'export',
   images: {
-    unoptimized: true,
     domains: ['pitch-slap-nadavlistingsync-nadavlistingsyncs-projects.vercel.app'],
     formats: ['image/avif', 'image/webp'],
   },
