462c4ba83869472b6e76fb3017c8b76696824530
Nadavlistingsync <Benedeknadav@gmail.com>
2025-06-06 15:01:53 -0400

    chore: remove experimental.serverActions from next.config.js (enabled by default in Next.js 14+)

--- next.config.js
+++ next.config.js
@@ -4,10 +4,6 @@ const nextConfig = {
     domains: ['placehold.co'],
     unoptimized: true,
   },
-  // Enable hybrid rendering
-  experimental: {
-    serverActions: true,
-  },
 };
 
 module.exports = nextConfig; 
